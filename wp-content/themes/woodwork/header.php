6dd41200f7df3b0da4946d94aff8ccdad14744c5
Elimrtnz <elimartineztwenty5@gmail.com>
2018-08-26 18:22:44 -0700

    Add home page

--- wp-content/themes/woodwork/header.php
+++ wp-content/themes/woodwork/header.php
@@ -28,27 +28,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'woodwork' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$woodwork_description = get_bloginfo( 'description', 'display' );
-			if ( $woodwork_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $woodwork_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+
+		<div class="mobile_nav" data-menu="off">
+        	<a class="menu-toggle" href="javascript:void(0);"></a>
+      	</div>
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'woodwork' ); ?></button>
+			
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
